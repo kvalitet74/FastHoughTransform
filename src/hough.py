0ee9fb03e837cb755dc0b2e5effd5c34c8bb8d9b
Dmitry Utkin <luckydim@mail.ru>
2024-10-28 22:34:02 +0500

    Update hough.py

--- src/hough.py
+++ src/hough.py
@@ -6,7 +6,7 @@ There are methods to compute the transform, strictly find lines and draw them on
 
 import numpy as np
 import pathlib
-from Pillow.Image import Image as PIL_Image
+from Pillow import Image as PIL_Image
 from Pillow import Image, ImageDraw
 import cv2 as cv
 from scipy import ndimage
